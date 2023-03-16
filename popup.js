9b65d68d1c9026cd84fe6256b846739bebf539c5
Mike <mike.s3crypto@gmail.com>
2023-03-16 23:55:26 +0000

    Added DOMContent Loaded listener

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
-chrome.runtime.sendMessage(
-  { action: "getLoadTime" },
-  function (response) {
-    document.getElementById("loadTime").textContent = `Load time: ${response.loadTime} ms`;
-  }
-);
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.runtime.sendMessage(
+    { action: "getLoadTime" },
+    function (response) {
+      document.getElementById("loadTime").textContent = `Load time: ${
+        response.loadTime === "N/A" ? "N/A" : response.loadTime + " ms"
+      }`;
+    }
+  );
+});
