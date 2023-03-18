cb13ee0785c2c2aac4eaaa3b4ccd12675663061c
Mike <mike.s3crypto@gmail.com>
2023-03-18 01:22:55 +0000

    Added Broken link Check

--- content.js
+++ content.js
@@ -8,3 +8,31 @@ const observer = new PerformanceObserver((entryList) => {
 });
 
 observer.observe({ entryTypes: ['navigation'] });
+
+// Broken Link Checker
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'check_broken_links') {
+    checkBrokenLinks();
+  }
+});
+
+async function checkBrokenLinks() {
+  const links = Array.from(document.querySelectorAll('a[href]'));
+  const brokenLinks = [];
+
+  for (const link of links) {
+    try {
+      const response = await fetch(link.href, { method: 'HEAD' });
+      if (!response.ok) {
+        brokenLinks.push({ url: link.href, status: response.status });
+      }
+    } catch (error) {
+      brokenLinks.push({ url: link.href, status: 'Error' });
+    }
+  }
+
+  chrome.runtime.sendMessage({
+    message: 'broken_links_report',
+    data: brokenLinks
+  });
+}
--- popup.js
+++ popup.js
@@ -7,4 +7,34 @@ document.addEventListener("DOMContentLoaded", function () {
       }`;
     }
   );
+
+  const checkLinksButton = document.getElementById('check-links');
+
+  checkLinksButton.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { message: 'check_broken_links' });
+    });
+  });
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'broken_links_report') {
+    displayBrokenLinks(request.data);
+  }
+});
+
+function displayBrokenLinks(brokenLinks) {
+  const resultsContainer = document.getElementById('results');
+  resultsContainer.innerHTML = '';
+
+  if (brokenLinks.length > 0) {
+    brokenLinks.forEach((link) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${link.url} - ${link.status}`;
+      resultsContainer.appendChild(listItem);
+    });
+  } else {
+    resultsContainer.textContent = 'No broken links found.';
+  }
+}
+
