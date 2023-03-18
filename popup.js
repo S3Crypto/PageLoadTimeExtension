85781552a11ef9c52d033afad7921cd3ee6156d0
Mike <mike.s3crypto@gmail.com>
2023-03-18 01:42:27 +0000

    Formatted Results from Check Broken links

--- popup.js
+++ popup.js
@@ -27,11 +27,28 @@ function displayBrokenLinks(brokenLinks) {
   const resultsContainer = document.getElementById('results');
   resultsContainer.innerHTML = '';
 
+  const list = document.createElement('ul');
+  list.classList.add('broken-links-list');
+  resultsContainer.appendChild(list);
+
   if (brokenLinks.length > 0) {
     brokenLinks.forEach((link) => {
       const listItem = document.createElement('li');
-      listItem.textContent = `${link.url} - ${link.status}`;
-      resultsContainer.appendChild(listItem);
+      listItem.classList.add('broken-link-item');
+      
+      const linkText = document.createElement('a');
+      linkText.classList.add('broken-link-text');
+      linkText.href = link.url;
+      linkText.target = '_blank';
+      linkText.textContent = link.url;
+      listItem.appendChild(linkText);
+      
+      const linkStatus = document.createElement('span');
+      linkStatus.classList.add('broken-link-status');
+      linkStatus.textContent = ` - ${link.status}`;
+      listItem.appendChild(linkStatus);
+
+      list.appendChild(listItem);
     });
   } else {
     resultsContainer.textContent = 'No broken links found.';
